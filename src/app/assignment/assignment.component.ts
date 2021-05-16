666b4bdf5e7a27d79ca3bf3827a45971129606d8
dadabhau <dadabhaut@ithena.ai>
2021-05-16 15:18:13 +0530

    Angular Basic Completed

--- src/app/assignment/assignment.component.ts
+++ src/app/assignment/assignment.component.ts
@@ -6,7 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./assignment.component.css']
 })
 export class AssignmentComponent implements OnInit {
+  //Assignment 1
   username = "";
+
+  //Assignment 2
+  showSecret = false;
+  log = [];
+  logAdd = [];
+
+  onToggleDetails(){
+    this.showSecret = !this.showSecret;
+    this.log.push(this.log.length + 1);
+    this.logAdd.push(new Date());
+  }
+  onToggleDetailsIndex(){
+    this.logAdd.push(new Date());
+  }
+
   constructor() { }
 
   ngOnInit(): void {
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -7,9 +7,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServersComponent implements OnInit {
 
-  constructor() { }
+  //Two way Data Binding
+  serversTitle = "";
+  serversCreationStatus = "NO Server was created!";
+  allowNewServer = false;
+  serversCreated = false;
+  serverStatus:string = 'Offline';
+  serverId:number = 10;
+
+  serverName = ['testserver', 'testserver2'];
+
+  constructor() {
+    // Property Binding
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+    this.serverStatus = Math.random() > 0.5 ? "online" : "offline";
+   }
 
   ngOnInit(): void {
   }
 
+   // Combining all Forms of Databinding
+   onCreateServers(){
+     this.serversCreated = true;
+     this.serverName.push(this.serversTitle);
+    this.serversCreationStatus = "Server was created name is " + this.serversTitle;
+  }
+
+
+  getServerStatus() {
+    return this.serverStatus;
+  }
+  getColor(){
+    return this.serverStatus === 'online' ? 'green' : 'red';
+  }
 }
